1cb503012fd751c65e55c68a1870b7d348d622ed
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-10-09 16:14:08 -0300

    inicio del proyecto chat app

---
+++ lib/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/pages/loading_page.dart';
+import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/pages/register_page.dart';
+import 'package:chat_app/pages/usuarios_page.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'usuarios' : (_) => UsuariosPage(),
+  'chat' : (_) => ChatPage(),
+  'login' : (_) => LoginPage(),
+  'register' : (_) => RegisterPage(),
+  'loading' : (_) => LoadingPage(),
+};
