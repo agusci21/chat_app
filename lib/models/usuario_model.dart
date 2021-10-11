2e1d9b67054f6959d3515f579a3730d539f463c7
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-10-11 10:50:13 -0300

    chat inicio

---
+++ lib/models/usuario_model.dart
@@ -0,0 +1,16 @@
+
+
+class Usuario{
+  bool online;
+  String email;
+  String nombre;
+  String uid;
+
+  Usuario({
+    required this.online,
+    required this.email,
+    required this.nombre,
+    required this.uid
+  });
+
+}
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,9 +6,54 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Chat'),
-     ),
+
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+
+            Row(
+              children: [
+                Spacer(),
+                CircleAvatar(
+                  child: Text('Te', style: TextStyle(fontSize: 12),),
+                  backgroundColor: Colors.blueAccent.shade100,
+                  maxRadius: 16,
+                ),
+                Spacer()
+              ],
+            ),
+
+            SizedBox(height: 5,),
+            Text('Agustin Cola', style: TextStyle(color: Colors.black87, fontSize: 12),)
+          ],
+        ),
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
+              )
+            ),
+
+            Divider(height: 15, color: Colors.black,),
+
+            //TODO caja de texto
+
+            Container(
+              color: Colors.white,
+              height: 100,
+            )
+
+          ],
+        ),
+      ),
    );
   }
 }
